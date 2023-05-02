42c3fa615e2b85e415eca23cd5009f1ea804806c
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2023-05-02 14:41:30 +0200

    Learn NextJS course

---
+++ learnNextJS/pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima
+          amet dolor deserunt quos ipsa illo cum tempore reprehenderit ab
+          commodi aspernatur dicta odit debitis similique ducimus nemo, aliquam
+          nesciunt.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eligendi
+          eius dolorem rem, nesciunt vel aperiam, exercitationem qui amet alias
+          mollitia reprehenderit, est doloribus illum animi sequi harum magni
+          labore.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
